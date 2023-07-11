b4a12d32a330e99e4d09683e164202627d96b288
Wei-Tse,Hsu <hsuweizte@gmail.com>
2023-07-11 13:41:13 +0800

    update

--- src/main/java/com/hsuweizte/api/TestApi.java
+++ src/main/java/com/hsuweizte/api/TestApi.java
@@ -8,7 +8,6 @@ import java.util.Date;
 import java.util.Map;
 
 @RestController
-public class TestApi {
     @PostMapping("/test")
     public String test(@RequestBody Map<String, String> email) {
         Date expireDate =
